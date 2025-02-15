2f7883a2a528d8f267658b60037873647cf61acb
Bo Shang <bo@shangsoftware.ru>
2025-02-15 09:17:48 -0500

    a

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 python3.11 print_lawsuit.py --firm_name "PDFSage Inc." \
   --case "Superior Court of California at Santa Clara" \
   --file case \
-  --exhibits esam1 twitchsecuritylinkedin.jpg
+  --exhibits esam1 twitchsecuritylinkedin.jpg esam2 twitchoauth.jpg esam3 simps1.jpg esam4 simps2.jpg
