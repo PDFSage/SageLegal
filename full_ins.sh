569929b3df76c3a607257b74673c97aef1021a9f
Bo Shang <bo@shangsoftware.ru>
2025-02-21 04:19:02 -0500

    sf

--- full_ins.sh
+++
@@ -1,9 +0,0 @@
-chmod +x lawsuit_generator.py
-./lawsuit_generator.py \
-    --firm_name "Your Law Firm" \
-    --case "Case Title Here" \
-    --output "lawsuit.pdf" \
-    --file "lawsuit_body.txt" \
-    --exhibits "caption1.txt" "image1.png" "caption2.txt" "image2.jpg" \
-    --index "index.pdf" \
-    --pickle "lawsuit.pickle"
